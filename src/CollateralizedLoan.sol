735a686f30c67e3ddeffba902802d300a6f3ba5a
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-03-01 17:22:22 +0200

    When deploying we set the ERC20 collateralToken

--- src/CollateralizedLoan.sol
+++ src/CollateralizedLoan.sol
@@ -2,10 +2,34 @@
 pragma solidity 0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // The borrowed asset is going to be Ether.
 // The collateral asset is going to be an ERC-20 token.
 //
 contract CollateralizedLoan is Ownable {
-    constructor(address initialOwner) Ownable(initialOwner) {}
+    struct LoanInfo {
+        address borrower;
+        uint256 borrowedAmount;
+        uint256 collateralAmount;
+        uint256 requestedAt;
+        bool paid;
+        // uint256 amount;
+        // uint256 collateral;
+        // uint256 interest;
+        // uint256 duration;
+        // uint256 start;
+        // uint256 end;
+        // address borrower;
+        // address lender;
+    }
+
+    IERC20 public collateralToken;
+
+    constructor(
+        address initialOwner,
+        IERC20 _collateralToken
+    ) Ownable(initialOwner) {
+        collateralToken = IERC20(_collateralToken);
+    }
 }
--- test/CollateralizedLoanTest.t.sol
+++ test/CollateralizedLoanTest.t.sol
@@ -3,14 +3,36 @@ pragma solidity 0.8.28;
 
 import {CollateralizedLoan} from "../src/CollateralizedLoan.sol";
 import {Test, console} from "forge-std/Test.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CollateralizedLoanTest is Test {
     // -------
     // Ownable
     // -------
     function test_ownerIsTheDeployer() public {
-        CollateralizedLoan load = new CollateralizedLoan(msg.sender);
+        IERC20 collateralToken = new ERC20Mock();
+
+        CollateralizedLoan load = new CollateralizedLoan(
+            msg.sender,
+            collateralToken
+        );
 
         assertEq(address(load.owner()), address(msg.sender));
+    }
+
+    // ----------------
+    // collateralToken
+    // ----------------
+    function test_onDeploymentWeSetPublicCollateralERC20Token() public {
+        address initialOwner = makeAddr("panos");
+        IERC20 erc20Token = new ERC20Mock();
+
+        CollateralizedLoan loan = new CollateralizedLoan(
+            initialOwner,
+            erc20Token
+        );
+
+        assertEq(address(loan.collateralToken()), address(erc20Token));
     }
 }
