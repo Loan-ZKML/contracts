60d097bdbc620e367d9e8cefc3adef060d9af393
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-03-01 17:33:17 +0200

    Use a 'setUp()' function to initialize state of the Test contract before each run

--- test/CollateralizedLoanTest.t.sol
+++ test/CollateralizedLoanTest.t.sol
@@ -7,32 +7,25 @@ import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CollateralizedLoanTest is Test {
+    CollateralizedLoan loan;
+    IERC20 collateralToken;
+
+    function setUp() public {
+        collateralToken = new ERC20Mock();
+        loan = new CollateralizedLoan(msg.sender, collateralToken);
+    }
+
     // -------
     // Ownable
     // -------
-    function test_ownerIsTheDeployer() public {
-        IERC20 collateralToken = new ERC20Mock();
-
-        CollateralizedLoan load = new CollateralizedLoan(
-            msg.sender,
-            collateralToken
-        );
-
-        assertEq(address(load.owner()), address(msg.sender));
+    function test_ownerIsTheDeployer() public view {
+        assertEq(address(loan.owner()), address(msg.sender));
     }
 
     // ----------------
     // collateralToken
     // ----------------
-    function test_onDeploymentWeSetPublicCollateralERC20Token() public {
-        address initialOwner = makeAddr("panos");
-        IERC20 erc20Token = new ERC20Mock();
-
-        CollateralizedLoan loan = new CollateralizedLoan(
-            initialOwner,
-            erc20Token
-        );
-
-        assertEq(address(loan.collateralToken()), address(erc20Token));
+    function test_onDeploymentWeSetPublicCollateralERC20Token() public view {
+        assertEq(address(loan.collateralToken()), address(collateralToken));
     }
 }
