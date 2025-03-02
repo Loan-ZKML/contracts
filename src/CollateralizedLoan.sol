338c5c04c57d8b28476965f19145ff8bf212ae72
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-03-02 08:57:25 +0200

    First test for requestLoan

--- src/CollateralizedLoan.sol
+++ src/CollateralizedLoan.sol
@@ -6,6 +6,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // The borrowed asset is going to be Ether.
 // The collateral asset is going to be an ERC-20 token.
+// The lender is the Smart Contract itself.
 //
 contract CollateralizedLoan is Ownable {
     struct LoanInfo {
@@ -24,23 +25,183 @@ contract CollateralizedLoan is Ownable {
         // address lender;
     }
 
-    IERC20 private s_collateralToken;
-    uint256 private s_interestRate; // percentage, e.g. 20%
+    IERC20 s_collateralToken;
+    uint256 s_interestRate; // percentage, e.g. 20%. It can be more than 100
+    uint256 s_minCollateralizationRatio; // percentage. It can be more than 100
+
+    mapping(address => LoanInfo) s_loans;
+
+    // --------------------------------------
+    // Events
+    // --------------------------------------
+    event LoanGranted(
+        address borrower,
+        uint256 borrowedAmount,
+        uint256 collateralAmount
+    );
+
+    event LoanRepaid(address borrower, uint256 repaidAmount);
+    //-----------------------------------------------------------------------
+
+    // --------------------------------------
+    // Errors
+    // --------------------------------------
+    error NotEnoughCollateralProvidedForBorrowedAmountError(
+        uint256 borrowedAmountRequested,
+        uint256 minCollateralizationRatio,
+        uint256 minimumCollateralRequired,
+        uint256 collateralProvided
+    );
+
+    error LenderDoesNotHaveEnoughEtherError(
+        uint256 amountRequested,
+        uint256 lenderBalance
+    );
+
+    error BorrowerDoesNotHaveEnoughCollateralError(
+        address borrower,
+        uint256 amountRequired,
+        uint256 borrowerBalance
+    );
+
+    error BorrowerHasUnpaidLoanError(
+        address borrower,
+        uint256 borrowedAmount,
+        uint256 requestedAt
+    );
+
+    error SendingEtherFailedError(
+        address sender,
+        address recipient,
+        uint256 value
+    );
+    // ----------------------------------------------------
 
     constructor(
         address _initialOwner,
         IERC20 _collateralToken,
-        uint256 _interestRate
+        uint256 _interestRate,
+        uint256 _minCollateralizationRatio
     ) Ownable(_initialOwner) {
         s_collateralToken = IERC20(_collateralToken);
         s_interestRate = _interestRate;
+        s_minCollateralizationRatio = _minCollateralizationRatio;
     }
 
+    // ----------------------------------------------------------
+    // Public View Functions
+    // ----------------------------------------------------------
     function collateralToken() public view returns (IERC20) {
         return s_collateralToken;
     }
 
     function interestRate() public view returns (uint256) {
         return s_interestRate;
+    }
+
+    function minimumCollateralRequired(
+        uint256 _borrowedAmount
+    ) public view returns (uint256) {
+        uint256 l_extraAmountToLiquidate = (_borrowedAmount *
+            s_minCollateralizationRatio) / 100;
+
+        return (_borrowedAmount + l_extraAmountToLiquidate);
+    }
+
+    // ----------------------------------------------------------
+    // External Transactions
+    // ----------------------------------------------------------
+
+    // The caller is requesting +_borrowedAmount+ of Ether.
+    // To get the loan, they send +_collateralAmount+ of the
+    // +s_collateralToken+.
+    // There is a minimum amount of collateral that they should
+    // send. It is equal to the +_borrowedAmount+ increased by
+    // its +s_minCollateralizationRatio+.
+    //
+    function requestLoan(
+        uint256 _borrowedAmount,
+        uint256 _collateralAmount
+    ) external {
+        LoanInfo storage loanInfo = s_loans[msg.sender];
+        if (loanInfo.borrowedAmount > 0 && !loanInfo.paid) {
+            revert BorrowerHasUnpaidLoanError({
+                borrower: msg.sender,
+                borrowedAmount: loanInfo.borrowedAmount,
+                requestedAt: loanInfo.requestedAt
+            });
+        }
+
+        uint256 l_minimumCollateralRequired = minimumCollateralRequired(
+            _borrowedAmount
+        );
+
+        if (_collateralAmount < l_minimumCollateralRequired) {
+            revert NotEnoughCollateralProvidedForBorrowedAmountError({
+                borrowedAmountRequested: _borrowedAmount,
+                minCollateralizationRatio: s_minCollateralizationRatio,
+                minimumCollateralRequired: l_minimumCollateralRequired,
+                collateralProvided: _collateralAmount
+            });
+        }
+
+        if (address(this).balance < _borrowedAmount) {
+            revert LenderDoesNotHaveEnoughEtherError({
+                amountRequested: _borrowedAmount,
+                lenderBalance: address(this).balance
+            });
+        }
+
+        uint256 borrowerCollateralBalance = s_collateralToken.balanceOf(
+            msg.sender
+        );
+
+        if (borrowerCollateralBalance < _collateralAmount) {
+            revert BorrowerDoesNotHaveEnoughCollateralError({
+                borrower: msg.sender,
+                amountRequired: _collateralAmount,
+                borrowerBalance: borrowerCollateralBalance
+            });
+        }
+
+        // we take from the borrower and we move Collateral to the contract
+        // But has the +msg.sender+ approved the +CollateralizedLoan+ to
+        // get money/collateralToken from +msg.sender+ and put it to itself?
+        //
+        s_collateralToken.transferFrom(
+            msg.sender,
+            address(this),
+            _collateralAmount
+        );
+
+        _sendEthersTo(payable(msg.sender), _borrowedAmount);
+
+        s_loans[msg.sender] = LoanInfo({
+            borrower: msg.sender,
+            borrowedAmount: _borrowedAmount,
+            collateralAmount: _collateralAmount,
+            requestedAt: block.timestamp,
+            paid: false
+        });
+
+        emit LoanGranted(msg.sender, _borrowedAmount, _collateralAmount);
+    }
+
+    // -------
+    // private
+    // -------
+
+    function _sendEthersTo(
+        address payable _recipient,
+        uint256 _amount
+    ) private {
+        (bool sent, ) = _recipient.call{value: _amount}("");
+        if (!sent) {
+            revert SendingEtherFailedError({
+                sender: address(this),
+                recipient: _recipient,
+                value: _amount
+            });
+        }
     }
 }
--- test/CollateralizedLoanTest.t.sol
+++ test/CollateralizedLoanTest.t.sol
@@ -4,19 +4,29 @@ pragma solidity 0.8.28;
 import {CollateralizedLoan} from "../src/CollateralizedLoan.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CollateralizedLoanTest is Test {
-    CollateralizedLoan loan;
-    IERC20 collateralToken;
+    CollateralizedLoan s_loan;
+    ERC20Mock s_collateralToken;
     uint256 constant INTEREST_RATE = 20; // 20%
+    uint256 constant MIN_COLLATERALIZATION_RATIO = 10; // ?
 
     function setUp() public {
-        collateralToken = new ERC20Mock();
-        loan = new CollateralizedLoan(
+        console.log(
+            "CollateralizedLoanTest#setUp(): msg.sender = ",
+            msg.sender
+        );
+        console.log(
+            "CollateralizedLoanTest#setUp(): address(this) = ",
+            address(this)
+        );
+
+        s_collateralToken = new ERC20Mock();
+        s_loan = new CollateralizedLoan(
             msg.sender,
-            collateralToken,
-            INTEREST_RATE
+            s_collateralToken,
+            INTEREST_RATE,
+            MIN_COLLATERALIZATION_RATIO
         );
     }
 
@@ -24,20 +34,74 @@ contract CollateralizedLoanTest is Test {
     // Ownable
     // -------
     function test_ownerIsTheDeployer() public view {
-        assertEq(address(loan.owner()), address(msg.sender));
+        assertEq(address(s_loan.owner()), address(msg.sender));
     }
 
     // ------------------
     // collateralToken()
     // ------------------
     function test_onDeploymentWeSetPublicCollateralERC20Token() public view {
-        assertEq(address(loan.collateralToken()), address(collateralToken));
+        assertEq(address(s_loan.collateralToken()), address(s_collateralToken));
     }
 
     // -------------
     // interestRate()
     // -------------
     function test_interestRate_returnsTheInterestRate() public view {
-        assertEq(loan.interestRate(), INTEREST_RATE);
+        assertEq(s_loan.interestRate(), INTEREST_RATE);
+    }
+
+    // ----------------------------
+    // minimumCollateralRequired()
+    // ----------------------------
+    function test_returnsTheMinimumCollateralRequired() public view {
+        uint256 borrowedAmount = 100 ether;
+        uint256 result = s_loan.minimumCollateralRequired(borrowedAmount);
+        uint256 expectedResult = borrowedAmount +
+            (borrowedAmount * MIN_COLLATERALIZATION_RATIO) /
+            100;
+
+        assertEq(result, expectedResult);
+    }
+
+    // -----------------------------
+    // requestLoan()
+    // -----------------------------
+    function test_requestLoan_whenBorrowerHasUnpaidLoan_itReverts() public {
+        // TODO: Maybe I could use +beforeTestSetup+ to set up
+        // some transaction state?
+        address borrower = makeAddr("panos");
+        uint256 borrowedAmount = 10 ether;
+        uint256 collateralAmount = 30 ether;
+
+        // the lender needs to have enough in order to lend
+        vm.deal(address(s_loan), borrowedAmount);
+
+        // the borrower needs to have enough in order to send collateral
+        s_collateralToken.mint(borrower, collateralAmount);
+
+        // the borrower needs to have given the CollateralizedLoan
+        // smart contract the allowance to transfer collateral from their
+        // account to the smart contract account.
+        vm.prank(borrower);
+        s_collateralToken.approve(address(s_loan), collateralAmount);
+
+        vm.prank(borrower);
+        s_loan.requestLoan(borrowedAmount, collateralAmount);
+
+        // ----
+        // fire
+        // ----
+
+        vm.prank(borrower);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                CollateralizedLoan.BorrowerHasUnpaidLoanError.selector,
+                borrower,
+                borrowedAmount,
+                block.timestamp
+            )
+        );
+        s_loan.requestLoan(borrowedAmount, collateralAmount);
     }
 }
