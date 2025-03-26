4599bfd4b51dfef170adafa755d2b6f3e2c1738b
arcadiogm <arcadiogm.com@gmail.com>
2025-03-26 18:27:17 +0100

    Update prove.sh

--- script/prove.sh
+++ script/prove.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+# Causes an exit if the Foundry process (which executes the ZKCreditScript.s.sol smart contract as a script) 
+# generates a non-zero return code (error).
 set -e
 
+# Foundry executes ZKCreditScript.s.sol smart contract as a script
 # Anvil must be running
 forge script script/ZKCreditScript.s.sol --rpc-url anvil --broadcast
